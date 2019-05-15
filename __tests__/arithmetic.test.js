4b7b66aece5c81ab1e58b7da67ba5d8fd747b243
Joé Jemmely <joe.jemmely@gmail.com>
2019-05-15 08:58:57 -0700

    refactor artihmetic.test.js

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,18 +2,20 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(arithmetic.add([1, 2])).toBe(3);
-});
+describe('Arithmetic functions', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(arithmetic.add([1, 2])).toBe(3);
+  });
 
-test('subtract 1 - 2 to equal -1', () => {
-  expect(arithmetic.subtract([1, 2])).toBe(-1);
-});
+  it('subtract 1 - 2 to equal -1', () => {
+    expect(arithmetic.subtract([1, 2])).toBe(-1);
+  });
 
-test('multiply 3 + 2 to equal 3', () => {
-  expect(arithmetic.multiply([3, 2])).toBe(6);
-});
+  it('multiply 3 + 2 to equal 3', () => {
+    expect(arithmetic.multiply([3, 2])).toBe(6);
+  });
 
-test('divide 9 by 3 to equal 3', () => {
-  expect(arithmetic.divide(9, 3)).toBe(3);
+  it('divide 9 by 3 to equal 3', () => {
+    expect(arithmetic.divide(9, 3)).toBe(3);
+  });
 });
