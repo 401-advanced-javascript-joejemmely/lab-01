816f43e60b7b20ee1c648f6291809d40cba56fe5
Joé Jemmely <joe.jemmely@gmail.com>
2019-05-20 21:55:13 -0700

    Update

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ console.log(math.add([1, 3, 6])); // 10
 console.log(math.subtract([1, 3, 6])); // -8
 console.log(math.multiply([2, 3, 6])); // 36
 console.log(math.divide(9, 3)); // 3
-console.log(math.divide(9, 0)); // 3
+// console.log(math.divide(9, 0)); // 3
